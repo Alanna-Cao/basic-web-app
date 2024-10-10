7c8d881940bcf83092c7c0c28b247f87fbdeea37
Alanna Cao <alannac@andrew.cmu.edu>
2024-10-10 10:00:11 -0400

    added sum

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -13,10 +13,20 @@ export default function QueryProcessor(query: string): string {
     )
   }
 
-  if (query.toLowerCase().includes("your name")) {
+  if (query.toLowerCase().includes("name")) {
     return (
       "alanna"
     )
+  }
+
+  const plusRegex = /(\d+)\s+plus\s+(\d+)/i;
+  const match = query.match(plusRegex);
+  if (match) {
+    const num1 = parseInt(match[1], 10);
+    const num2 = parseInt(match[2], 10);
+    const sum = num1 + num2;
+    return `${sum}`;
+    // return `${num1} plus ${num2} equals ${sum}`;
   }
 
   return "";
